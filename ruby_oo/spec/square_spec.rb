b576c667321ea1416640a1c266a4b2aad9f758cb
Nando <fernando.ribeiro@dcx.ufpb.br>
2024-06-19 21:19:18 -0300

    feat: criando testes para o quadrado

--- ruby_oo/spec/square_spec.rb
+++ ruby_oo/spec/square_spec.rb
@@ -4,7 +4,12 @@ require 'square'
 RSpec.describe Square do
     #forma de chamar a função pra execução
     describe "#area" do
-        square = Square.new
-        expect(square.area).to eq(25)
+        it 'Square is constant' do
+            expect(defined?(Square)).to eq('constant')
+        end
+        it "return area Square L^2" do
+            square = Square.new(5)
+            expect(square.area).to eq(25)
+        end
     end
 end
